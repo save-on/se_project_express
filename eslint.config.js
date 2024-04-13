b14ad0fe3c8c4d015f38d3d9fc7717479736a13a
Sayvon <sedmon033@gmail.com>
2024-04-12 21:22:25 -0500

    eslint questions

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-console": "off",
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+  },
+};
